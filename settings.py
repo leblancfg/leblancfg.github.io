71715c37eb5b1521ca67a3d75fae5019a55cfe45
François Leblanc <leblancfg@gmail.com>
2018-05-29 20:51:36 -0400

    first push

---
+++ settings.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-P
+
+AUTHOR = u"François Leblanc"
+SITEURL = "http://leblancfg.com"
+SITENAME = u"François Leblanc"
+LOCALE = 'en_US.utf8'
+TIMEZONE = 'America/Toronto'
+
+DISPLAY_PAGES_ON_MENU = True
+WITH_PAGINATION = True
+DEFAULT_PAGINATION = 7
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+RELATIVE_URLS = False
+DELETE_OUTPUT_DIRECTORY = True
+
+TWITTER_USERNAME = "leblancfg1"
+GITHUB_URL = "https://github.com/leblancfg"
+DISQUS_SITENAME = "leblancfg"
+
+SOCIAL = (
+    ("LinkedIn", "https://www.linkedin.com/in/fran%C3%A7ois-leblanc-07294b106"),
+    ("GitHub", "https://github.com/leblancfg"),
+    ("Twitter", "http://twitter.com/leblancfg1"),
+)
+
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
