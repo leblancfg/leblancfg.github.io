0a872776b0e3104afe4953b47b15449f855b8317
François Leblanc <leblancfg@gmail.com>
2018-05-29 21:05:19 -0400

    changed http to https in publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://leblancfg.github.io'
+SITEURL = 'https://leblancfg.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
