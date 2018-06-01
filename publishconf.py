3e3ec2b8f9809b907d88703c6be6a98b1234a419
Francois Leblanc <leblancfg@gmail.com>
2018-06-01 09:00:27 -0400

    https -> http for now

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://leblancfg.com'
+SITEURL = 'http://leblancfg.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
