1a283483d10dfe67ab8d3e174d93d671fb372528
François Leblanc <leblancfg@gmail.com>
2018-06-04 07:19:42 -0400

    social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,11 +20,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-# LINKS = (('Pelican', 'http://getpelican.com/'),
-#          ('Python.org', 'http://python.org/'),
-#          ('Jinja2', 'http://jinja.pocoo.org/'),
-#          ('You can modify those links in your config file', '#'),)
-#
+LINKS = (('Github', 'https://github.com/leblancfg'),
+        ('Kaggle', 'https://www.kaggle.com/leblancfg'),
+        ('Twitter', 'https://twitter.com/leblancfg1'),)
+
 # Social widget
 # SOCIAL = (('You can add links in your config file', '#'),
 #           ('Another social link', '#'),)
