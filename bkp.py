8a4e9aa79eb3310465c38dde3f5f7478439c1e3c
François Leblanc <leblancfg@gmail.com>
2018-06-27 00:10:09 -0400

    Updated content

---
+++ bkp.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'François Leblanc'
+SITENAME = 'leblancfg.com'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Toronto'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+# LINKS = (('leblancfg on Github', 'https://github.com/leblancfg'),
+#         ('leblancfg on Kaggle', 'https://www.kaggle.com/leblancfg'),
+#         ('leblancfg1 on Twitter', 'https://twitter.com/leblancfg1'),)
+
+# Social widget
+ICONS_PATH = 'images/icons'
+SOCIAL_ICONS = [
+            ('mailto:leblancfg@gmail.com', 'Email me', 'fa-envelope'),
+                ('http://github.com/leblancfg', 'Browse my projects', 'fa-github'),
+                ('https://twitter.com/leblancfg1', 'Tweet at me', 'fa-twitter'),
+                    ('https://www.linkedin.com/in/fran%C3%A7ois-leblanc-07294b106/', 'View Linkedin Profile', 'fa-linkedin'),
+                        ('/atom.xml', 'Atom Feed', 'fa-rss'),
+                        ]
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['ipynb.markup']
+
+# Define Links
+STATIC_PATHS = ['img', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
+
+# Analytics
+GOOGLE_ANALYTICS = 'UA-120300361-1'
+
+# Theme
+THEME = 'Flex'
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,37 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
 
 AUTHOR = 'François Leblanc'
-SITENAME = 'leblancfg.com'
-SITEURL = ''
+SITEURL = 'http://localhost:8000'
+SITENAME = "leblancfg.com"
+SITETITLE = AUTHOR
+SITESUBTITLE = 'Data Science, Geospatial Python, Space Stuff'
+SITEDESCRIPTION = '%s\'s Thoughts and Writings' % AUTHOR
+SITELOGO = 'https://avatars0.githubusercontent.com/u/15659410?s=400&u=e3bc92486becb34e77028eef0f4dfc302540fcb3&v=4'
+FAVICON = '/img/icons/favicon.ico'
+BROWSER_COLOR = '#330033'
+# PYGMENTS_STYLE = 'monokai'
 
+ROBOTS = 'index, follow'
+
+THEME = 'Flex'
 PATH = 'content'
+TIMEZONE = 'America/Montreal'
 
-TIMEZONE = 'America/Toronto'
-
+I18N_TEMPLATES_LANG = 'en'
 DEFAULT_LANG = 'en'
+OG_LOCALE = 'en_US'
+LOCALE = 'en_US'
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+DATE_FORMATS = {
+    'en': '%B %d, %Y',
+}
+
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('leblancfg on Github', 'https://github.com/leblancfg'),
-        ('leblancfg on Kaggle', 'https://www.kaggle.com/leblancfg'),
-        ('leblancfg1 on Twitter', 'https://twitter.com/leblancfg1'),)
+USE_FOLDER_AS_CATEGORY = False
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
 
-# Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-#           ('Another social link', '#'),)
+LINKS = (('Kaggle', 'https://kaggle.com/leblancfg'),)
+
+SOCIAL = (('linkedin', 'https://www.linkedin.com/in/fran%C3%A7ois-leblanc-07294b106/'),
+          ('github', 'https://github.com/leblancfg'),
+          ('twitter', 'https://twitter.com/leblancfg1'),
+          ('rss', 'https://leblancfg.com/feeds/all.atom.xml'))
+
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
+
+CC_LICENSE = {
+    'name': 'Creative Commons Attribution-ShareAlike',
+    'version': '4.0',
+    'slug': 'by-sa'
+}
+
+COPYRIGHT_YEAR = 2018
 
 DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
 
 MARKUP = ('md', 'ipynb')
 
@@ -44,6 +69,3 @@ EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
 
 # Analytics
 GOOGLE_ANALYTICS = 'UA-120300361-1'
-
-# Theme
-THEME = 'Flex'
