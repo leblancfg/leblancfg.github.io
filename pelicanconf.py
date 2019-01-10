f6d793f69bb3810a94b2ecb191b8e451f1f45ea9
François Leblanc <francois.leblanc7@canada.ca>
2019-01-10 08:39:57 -0500

    Updated content

--- pelicanconf.py
+++ pelicanconf.py
@@ -69,3 +69,17 @@ EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}, }
 
 # Analytics
 GOOGLE_ANALYTICS = 'UA-120300361-1'
+
+# Adsense
+GOOGLE_ADSENSE = {
+    'ca_id': 'ca-pub-9414058741554736',    # Your AdSense ID
+    'page_level_ads': True,          # Allow Page Level Ads (mobile)
+    'ads': {
+        'aside': '1234561',          # Side bar banner (all pages)
+        'main_menu': '1234562',      # Banner before main menu (all pages)
+        'index_top': '1234563',      # Banner after main menu (index only)
+        'index_bottom': '1234564',   # Banner before footer (index only)
+        'article_top': '1234565',    # Banner after article title (article only)
+        'article_bottom': '1234566', # Banner after article content (article only)
+    }
+}
